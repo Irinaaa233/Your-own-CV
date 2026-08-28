363d578608a88249a2fa569fbeaafcb2bc3d2f3a
Irinaaa233 <rxwang13@gmail.com>
2026-08-29 02:08:53 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import Welcome from "@/pages/Welcome";
 import { useResumeStore } from "@/store/useResumeStore";
